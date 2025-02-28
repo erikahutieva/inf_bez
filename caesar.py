91cb883e172e959c1c989d2db6f910cd0e95c3e3
Erika <erika@MacBook-Pro-Erika.local>
2025-02-28 13:20:52 +0300

    лаба1

--- caesar.py
+++ caesar.py
@@ -1,46 +1,42 @@
 import typing as tp
-
-
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in plaintext:
+        if 'A' <= i <= 'Z':
+            ciphertext += chr((ord(i) - ord('A') + shift) % 26 + ord('A'))
+        elif 'a' <= i <= 'z':
+            ciphertext += chr((ord(i) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            ciphertext += i
     return ciphertext
 
-
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in ciphertext:
+        if 'A' <= i <= 'Z':
+            plaintext += chr((ord(i) - ord('A') - shift) % 26 + ord('A'))
+        elif 'a' <= i <= 'z':
+            plaintext += chr((ord(i) - ord('a') - shift) % 26 + ord('a'))
+        else:
+            plaintext += i
     return plaintext
 
-
 def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
     best_shift = 0
-    # PUT YOUR CODE HERE
+    plaintext = ""
+    while plaintext not in dictionary:
+        best_shift += 1
+        plaintext = decrypt_caesar(ciphertext, best_shift)
     return best_shift
+
+print(encrypt_caesar("PYTHON"))  
+print(encrypt_caesar("python"))  
+print(encrypt_caesar("Python3.6")) 
+print(encrypt_caesar("")) 
+
+print(decrypt_caesar("SBWKRQ"))
+print(decrypt_caesar("sbwkrq"))
+print(decrypt_caesar("Sbwkrq3.6"))
+print(decrypt_caesar(""))
+
+print(caesar_breaker_brute_force("SBWKRQ", {"PYTHON", "python", "Python3.6", "sbwkrq", "Sbwkrq3.6"}))
